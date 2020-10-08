fef6efe49f7e479a8c7db0cd46eddfa6122be37e
Michael Kim <m.kim4247@gmail.com>
2020-10-08 20:28:38 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,26 +1,23 @@
-def roll_call_dwarves(array)
-  array.each_with_index do |dwarf, i|
-    puts "#{i + 1} #{dwarf}"
-  end 
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index do |dwarf, index|
+    puts "#{index+1} #{dwarf}"
+  end
 end
 
-def summon_captain_planet(array)
-  array.collect do |call|
+def summon_captain_planet(calls)
+  calls.map do |call|
     call.capitalize + "!"
   end
 end
 
-def long_planeteer_calls(array)
-  array.any? do |word|
-    word.length > 4
-  end 
+def long_planeteer_calls(calls_arr)
+  calls_arr.any? do |call|
+    call.length > 4
+  end
 end
 
-def find_the_cheese(array)
-  cheese_types = ["cheddar", "gouda", "camembert"]
-  array.find do |cheese|
-    if cheese == "cheddar" || cheese == "gouda" || cheese == "camembert"
-      cheese 
-    end 
-  end 
+def find_the_cheese(ingredients)
+  ingredients.find do |cheese|
+    cheese == "cheddar" || cheese == "gouda" || cheese == "camembert"
+  end
 end
